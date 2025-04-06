f834b86ed4ee9788d93897b5f4c9291ed2668b62
DominkoMilic <dmilic04@fesb.hr>
2025-04-06 15:22:41 +0200

    deployment additions

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,35 @@ const session = require("express-session");
 const passport = require("passport");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoute");
-require("./config/passport");
 const userDataRoute = require("./routes/userDataRoute");
 const addPlanRoute = require("./routes/addPlanRoute");
 const deletePlanRoute = require("./routes/deletePlanRoute");
 const plansRoute = require("./routes/findAllPlansRoute");
 const editPlanRoute = require("./routes/editPlanRoute");
 
+const MongoStore = require("connect-mongo");
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "your-secret-key",
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      ttl: 14 * 24 * 60 * 60,
+    }),
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24,
+    },
   })
 );
 
